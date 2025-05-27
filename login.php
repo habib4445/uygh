081b6ce520e2d3a15126ed71de7c2a227a628286
habib4445 <0124habibur@gmail.com>
2025-05-27 11:11:12 +0600

    login part

--- login.php
+++ login.php
@@ -201,7 +201,30 @@ i {
 <head>
     <body>
 
-Habib
+<section class="bg-light vh-100n d-flex">
+    <div class="col-3 m-auto">
+        <div class="card">
+            <div class="card-body">
+                <div class="text-center"> <span class="fa-stack fa-2x">
+                     <i class="fa fa-circle fa-stack-2x"></i>
+                     <i class="fa fa-user fa-stack-1x text-light"></i> 
+                     
+                    </span> </div>
+                <form action="">
+                    <div class="md-form">
+                        <input type="email" id="email" class="form-control">
+                        <label for="email">Your Email</label>
+                    </div>
+                    <div class="md-form">
+                        <input type="password" id="password" class="form-control">
+                        <label for="password">Your Password</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</section>
 
         
    <?php include('footer.php') ?>
