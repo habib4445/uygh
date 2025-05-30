dc4a3cf6e4801b83e43e960abcffb1a8f66c24ce
habib4445 <0124habibur@gmail.com>
2025-05-30 14:33:57 +0600

    again work

--- index.php
+++ index.php
@@ -2,30 +2,35 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-info">
   <div class="container-fluid ">
-    <a class="navbar-brand" href="#">SMS</a>
+    <a class="navbar-brand text-warning" href="#">SMS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link text-white" href="#">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link text-white" href="#">Pricing</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
+            <?php if (isset($_SESSION['login'])) { ?>
+          <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" dat-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user"></i>
+            </a>
+        
+          <ul class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
+          <?php } else { ?>
+            <a href="login.php" class="nav-link text-white"> <i class="fa fa-user mr-2"></i>login </a>
+            <?php } ?>
         </li>
       </ul>
     </div>
